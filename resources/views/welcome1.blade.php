db3bb478789a860ca1f0c48b24492bd57c1fe2e8
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2023-06-22 21:01:01 +0200

    some prob

---
+++ resources/views/welcome1.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>swiccher</title>
+    <style>
+        *{
+            margin: 0;
+            box-sizing: border-box;
+        }
+        a{
+            border: 2px solid black;
+            padding: 1rem;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <a href="./welcome.blade.php">page 1</a>
+    <a href="./welcome1.blade.php">page 2</a>
+    <a href="./welcome2.blade.php">page 3</a>
+</header>
+<h3>Pagina 2</h3>
+</body>
+</html>
